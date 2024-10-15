10df3420aa0d30af0c4cacec5b9310359212877c
janemir <janemiro2005@gmail.com>
2024-10-15 21:41:22 +0300

    добавила проверку наличия победителя

--- program.cs
+++ program.cs
@@ -44,6 +44,23 @@ class Program
         return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row, col] == ' ';
     }
 
+    // Проверка наличия победителя (ветка Win)
+    static bool CheckWin() 
+    {
+        // Проверка горизонталей и вертикалей
+        for (int i = 0; i < 3; i++) 
+        {
+            if (board[i, 0] == currentPlayer && board[i, 1] == currentPlayer && board[i, 2] == currentPlayer) return true;
+            if (board[0, i] == currentPlayer && board[1, i] == currentPlayer && board[2, i] == currentPlayer) return true;
+        }
+
+        // Проверка диагоналей
+        if (board[0, 0] == currentPlayer && board[1, 1] == currentPlayer && board[2, 2] == currentPlayer) return true;
+        if (board[0, 2] == currentPlayer && board[1, 1] == currentPlayer && board[2, 0] == currentPlayer) return true;
+
+        return false;
+    }
+
     // Основная логика игры
     static void PlayGame() 
     {
@@ -66,6 +83,14 @@ class Program
                 {
                     board[row, col] = currentPlayer; // Игрок может сделать ход только в пустую клетку
                     validMove = true;
+
+                    // Проверка на победу (ветка Win)
+                    if (CheckWin()) 
+                    {
+                        PrintBoard();
+                        Console.WriteLine($"Игрок {currentPlayer} победил!");
+                        gameOver = true;
+                    }
 
                     else 
                     {
